346626a25b16aef2ce308da834ad42329cf99315
Ilia <barbizone@gmail.com>
2020-10-30 17:44:28 +0200

    -

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,10 +3,12 @@ import { AuthService } from './auth.service';
 import { UsersModule } from '../users/users.module';
 import { PassportModule } from '@nestjs/passport';
 import { LocalStrategy } from './local.strategy';
+import { AuthController } from './auth.controller';
 
 @Module({
   imports: [UsersModule, PassportModule],
   providers: [AuthService, LocalStrategy],
+  controllers: [AuthController],
 })
 export class AuthModule {}
 
--- src/auth/local.strategy.ts
+++ src/auth/local.strategy.ts
@@ -10,6 +10,7 @@ export class LocalStrategy extends PassportStrategy(Strategy) {
   }
 
   async validate(username: string, password: string): Promise<any> {
+    console.debug('aa1122');
     const user = await this.authService.validateUser(username, password);
     if (!user) {
       throw new UnauthorizedException();
